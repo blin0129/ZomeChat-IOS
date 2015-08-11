7a25d67f002502296e869037fae6dbfe8c3f3dd1
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-08-11 14:58:42 -0700

    AppDelegate Organization

--- ZomeChat/AppDelegate.m
+++ ZomeChat/AppDelegate.m
@@ -13,6 +13,8 @@
     BOOL manuelLocationUpdate;
 }
 
+#pragma mark - Variables
+
 @synthesize window = _window;
 @synthesize locationManager=_locationManager;
 @synthesize socketIO;
@@ -25,6 +27,8 @@
 @synthesize imageCache;
 @synthesize version;
 @synthesize listeningPort;
+
+#pragma mark - UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -41,14 +45,50 @@
 //    serverURL = @"ec2-54-205-59-87.compute-1.amazonaws.com:1442";
     serverURL = @"localhost:1442";
     
-    [self initAppSetting];
+    [self initAppSettings];
     [self initLocationManager];
     [self connectServer];
     [GMSServices provideAPIKey:@"AIzaSyD_K5ZnON6GNk1KsNROdG3oI0NpDCj0MRc"];
     return YES;
 }
 
-- (void) initAppSetting
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
+}
+
+#pragma mark - Application Setup -
+
+- (void) initAppSettings
 {
     //Navation Bar Style
     self.window.tintColor = [UIColor whiteColor];
@@ -60,7 +100,7 @@
       [UIColor whiteColor],UITextAttributeTextShadowColor,
       nil]];
     [self setInitialConstraints];
-    [self initAlartMessages];
+    [self initAlertMessages];
     
     self.backgroundTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
         [[UIApplication sharedApplication] endBackgroundTask:self.backgroundTask];
@@ -71,6 +111,8 @@
     UIMenuItem * reportItem = [[UIMenuItem alloc] initWithTitle:@"Report" action:@selector(report:)];
     [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObjects:reportItem, nil]];
 }
+
+#pragma mark Server
 
 - (void) connectServer
 {
@@ -86,6 +128,24 @@
     NSLog(@"Reconnect server");
     [socketIO closeWithFast:@true];
     [socketIO connect];
+}
+
+#pragma mark CLLocationManagerDelegate and Helpers
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation * currentLocation = (CLLocation *)[locations lastObject];
+    if (currentLocation != nil)
+    {
+        lat = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+        lng = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+        if(manuelLocationUpdate){
+            [self.mainVC requestLocationUpdate];
+            [self.mainVC updateLocationOnMap];
+            manuelLocationUpdate = false;
+        }
+    }
+    [manager stopUpdatingLocation];
 }
 
 - (void) initLocationManager
@@ -128,23 +188,7 @@
     }
 }
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
-{
-    CLLocation * currentLocation = (CLLocation *)[locations lastObject];
-    if (currentLocation != nil)
-    {
-        lat = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-        lng = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
-        if(manuelLocationUpdate){
-            [self.mainVC requestLocationUpdate];
-            [self.mainVC updateLocationOnMap];
-            manuelLocationUpdate = false;
-        }
-    }
-    [manager stopUpdatingLocation];
-}
-
-- (void)initAlartMessages{
+- (void)initAlertMessages{
     self.commentPostAlertTitle = @"You are in \"Browse Mode\"";
     self.commentPostAlertMessage = @"Please log in to comment on the post";
     
@@ -175,6 +219,8 @@
     self.noTaggedFeedAlertTitle = @"No Match Found";
     self.noTaggedFeedAlertMessage = @"No tagged post around your area";
 }
+
+#pragma mark Constraints
 
 - (void)setInitialConstraints{
     self.changeUserImage = NO;
@@ -225,41 +271,6 @@
     self.chatroomConversation = NO;
     self.chatroomSendImage = NO;
     self.changeUsername = NO;
-}
-
-//For facebook API
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    return [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                          openURL:url
-                                                sourceApplication:sourceApplication
-                                                       annotation:annotation];
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
